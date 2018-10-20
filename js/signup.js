98c6bb638c1892c1a8dce98c4a248a022904954d
Gianfra <pguerrero002@ikasle.ehu.eus>
2018-10-20 22:02:28 +0200

    eiwi

---
+++ js/signup.js
@@ -0,0 +1,12 @@
+
+  function comprobardatos(){
+    var nombre = document.getElementById("nombre").value;
+
+    window.alert("hola");
+  }
+
+  function comprobarBD(){
+    var username = document.forms["identificacion"].username.value;
+    var password = document.forms["identificacion"].pass.value;
+    alert("username");
+  }
