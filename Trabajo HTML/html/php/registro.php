7e750089e3bb2e2fbacffbc714b54bc872cb5c4e
itrollyou <pguerrero002@ikasle.ehu.eus>
2018-10-20 21:25:32 +0200

    Remove

--- Trabajo HTML/html/php/registro.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $conexion = new mysqli("localhost", "root", "jsdfh", "COMPUSHOP");
-  if ($conexion->connect_errno){
-    echo "ERROR: (" . $conexion->connect_errno .")" . $conexion->connect_error;
-  }
-  $resultado = $conexion->query("INSERT INTO USUARIOS VALUES (nombre, apellidos, dni, telefono, fecha, email, password)");
-  mysqli_close($conexion);
-?>
