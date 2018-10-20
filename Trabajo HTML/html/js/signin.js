3e3a7cca2db2bce35ae043b06caa60e5acded935
andrea <andrea98.gar@gmail.com>
2018-10-20 20:13:07 +0200

    inicios

---
+++ Trabajo HTML/html/js/signin.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+
+  function comprobardatos(){
+    var nombre = document.getElementById("nombre").value;
+
+    window.alert(nombre);
+  }
+
+
+  function comprobarBD(){
+    var username = document.forms["identificacion"].username.value;
+    var password = document.forms["identificacion"].pass.value;
+    window.alert("username");
+  }
+
+</script>
---
+++ Trabajo HTML/html/php/registro.php
@@ -0,0 +1,8 @@
+<?php
+  $conexion = new mysqli("localhost", "root", "PON TU CONTRASENA", "COMPUSHOP");
+  if ($conexion->connect_errno){
+    echo "ERROR: (" . $conexion->connect_errno .")" . $conexion->connect_error;
+  }
+  $resultado = $conexion->query("INSERT INTO USUARIOS VALUES (nombre, apellidos, dni, telefono, fecha, email, password)");
+  mysqli_close($conexion);
+?>
