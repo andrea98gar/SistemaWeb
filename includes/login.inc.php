9aa5bfba9c0d192b6bf4ae640d997e6778ec7dfd
Andrea <agarcia683@ikasle.ehu.eus>
2019-01-17 00:52:44 +0100

    Logueo y monitorizacion insuficientes - Done

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -12,6 +12,12 @@ if (isset($_POST['login-submit'])) {
         header("Location: ../index.php?error=emptyfields");
         exit();
     } else {
+        //Intento conexion
+        $sql1 = "INSERT INTO LOGS  VALUES (?, ?, ?)";
+        $fecha = date('d/m/Y h:i:s A');
+        $stmt = mysqli_stmt_init($conexion);
+
+
         //Se comprueba si el usuario está en registrado en el sistema
         $sql = "SELECT * FROM USUARIOS WHERE Usuario='" . $usuario . "'";
         $result = $conexion->query($sql);
@@ -19,17 +25,52 @@ if (isset($_POST['login-submit'])) {
 
             //Se comprueba que la contraseña obtenida de la BBDD coincida con la introducida
             if ($contrasena !== $row['Contrasena']) {
+                //Almacenar intento de conexion fallido
+                $intento = "Fallido";
+                if (!mysqli_stmt_prepare($stmt, $sql1)) {
+                    header("Location: ../index.php?error=sqlerror");
+                    exit();
+                } else {
+                    //Enlazamos
+                    mysqli_stmt_bind_param($stmt, "sss", $usuario, $fecha, $intento);
+                    //Ejecutamos
+                    mysqli_stmt_execute($stmt);
+                }
                 header("Location: ../index.php?error=wrongpwd");
                 exit();
             } else {
                 //En una variable de sesión se guarda elusuario que se ha identificado.
                 $_SESSION['userId'] = $row['Usuario'];
+                //CIERRE DE SESIÓN TRAS 1 MINUTO DE INACTIVIDAD
                 $_SESSION['identificado'] = "si";
                 $_SESSION["ultimoAcceso"] = time();
+
+                //Almacenar intento de conexion exitoso
+                $intento = "Exitoso";
+                if (!mysqli_stmt_prepare($stmt, $sql1)) {
+                    header("Location: ../index.php?error=sqlerror");
+                    exit();
+                } else {
+                    //Enlazamos
+                    mysqli_stmt_bind_param($stmt, "sss", $usuario, $fecha, $intento);
+                    //Ejecutamos
+                    mysqli_stmt_execute($stmt);
+                }
+
                 header("Location: ../index.php?login=success");
                 exit();
             }
         } else {
+            $intento = "Fallido";
+            if (!mysqli_stmt_prepare($stmt, $sql1)) {
+                header("Location: ../index.php?error=sqlerror");
+                exit();
+            } else {
+                //Enlazamos
+                mysqli_stmt_bind_param($stmt, "sss", $usuario, $fecha, $intento);
+                //Ejecutamos
+                mysqli_stmt_execute($stmt);
+            }
             header("Location: ../index.php?login=wronguidpwd");
             exit();
         }
--- signup.php
+++ signup.php
@@ -24,6 +24,8 @@ require "header.php";
                 <input type="text" name="usuario" placeholder="Nombre de usuario" required><br><br>
                 <input type="password" name="contra1" placeholder="Contraseña" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required><br><br>
                 <input type="password" name="contra2" placeholder="Repita su contraseña" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required><br>
+            <!--<input type="password" name="contra1" placeholder="Contraseña"  required><br><br>
+                <input type="password" name="contra2" placeholder="Repita su contraseña" required><br>-->
                 <br><br>
 
                 <button type="submit" name="signup-submit"> Registrarse</button>
